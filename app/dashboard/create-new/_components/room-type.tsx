9a1ff7ed62a08bf712c11f2db8d2ce7db11f72d9
MarlonChi <marlonchiodelli@hotmail.com>
2024-11-24 15:31:23 -0300

    feat: add room types

--- app/dashboard/create-new/_components/room-type.tsx
+++ app/dashboard/create-new/_components/room-type.tsx
@@ -19,11 +19,15 @@ export const RoomType = ({ selectedRoomType }: RoomTypeProps) => {
           <SelectValue placeholder="Tipo de cômodo" />
         </SelectTrigger>
         <SelectContent>
-          <SelectItem value="livingRoom">Sala de estar</SelectItem>
-          <SelectItem value="bedroom">Quarto</SelectItem>
-          <SelectItem value="kitchen">Cozinha</SelectItem>
-          <SelectItem value="office">Escritório</SelectItem>
-          <SelectItem value="bathroom">Banheiro</SelectItem>
+          <SelectItem value="Sala de Estar">Sala de estar</SelectItem>
+          <SelectItem value="Sala de jantar">Sala de jantar</SelectItem>
+          <SelectItem value="Quarto">Quarto</SelectItem>
+          <SelectItem value="Suíte">Suíte</SelectItem>
+          <SelectItem value="Closet ">Closet </SelectItem>
+          <SelectItem value="Cozinha">Cozinha</SelectItem>
+          <SelectItem value="Escritório">Escritório</SelectItem>
+          <SelectItem value="Banheiro">Banheiro</SelectItem>
+          <SelectItem value="Lavanderia">Lavanderia</SelectItem>
         </SelectContent>
       </Select>
     </div>
