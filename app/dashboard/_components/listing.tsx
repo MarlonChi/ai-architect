07bb313dcdc861ff750f986995e54d09fff792be
MarlonChi <marlonchiodelli@hotmail.com>
2024-11-23 22:33:49 -0300

    feat: dashboard

--- app/dashboard/_components/listing.tsx
+++ app/dashboard/_components/listing.tsx
@@ -1,15 +1,48 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
 
 import { Button } from "@/components/ui/button";
 import { EmptyState } from "./empty-state";
+import { db } from "@/config/db";
+import { AiGeneratedImage } from "@/config/schema";
+import { RoomItem } from "./room-item";
+
+export interface RoomDetails {
+  id: number;
+  aiImage: string;
+  designType: string;
+  orgImage: string;
+  roomType: string;
+  userEmail: string | null;
+}
 
 export const Listing = () => {
   const { user } = useUser();
-  const [userRoomList, setUserRoomList] = useState([]);
+  const [userRoomList, setUserRoomList] = useState<RoomDetails[]>([]);
+
+  useEffect(() => {
+    const GetUserRoomList = async () => {
+      if (user?.primaryEmailAddress?.emailAddress) {
+        const result = await db
+          .select()
+          .from(AiGeneratedImage)
+          .where(
+            eq(
+              AiGeneratedImage.userEmail,
+              user.primaryEmailAddress.emailAddress
+            )
+          );
+
+        setUserRoomList(result);
+      }
+    };
+
+    GetUserRoomList();
+  }, [user]);
 
   return (
     <div>
@@ -20,7 +53,18 @@ export const Listing = () => {
         </Link>
       </div>
 
-      {userRoomList.length === 0 ? <EmptyState /> : <div></div>}
+      {userRoomList.length === 0 ? (
+        <EmptyState />
+      ) : (
+        <div className="mt-10">
+          <h2 className="font-medium text-primary text-2xl mb-8">Galeria</h2>
+          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {userRoomList.map((room: RoomDetails) => (
+              <RoomItem key={room.id} room={room} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
---
+++ app/dashboard/_components/room-item.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import ReactBeforeSliderComponent from "react-before-after-slider-component";
+import "react-before-after-slider-component/dist/build.css";
+
+import { RoomDetails } from "./listing";
+import { AiOutputDialog } from "./ai-output-dialog";
+
+interface RoomItemProps {
+  room: RoomDetails;
+}
+
+export const RoomItem = ({ room }: RoomItemProps) => {
+  const [openOutputDialog, setOpenOutputDialog] = useState(false);
+
+  const onClickHandler = () => {
+    setOpenOutputDialog(true);
+  };
+
+  return (
+    <div
+      className="shadow-md rounded-md cursor-pointer"
+      onClick={() => onClickHandler()}
+    >
+      <ReactBeforeSliderComponent
+        firstImage={{
+          imageUrl: room.aiImage,
+        }}
+        secondImage={{
+          imageUrl: room.orgImage,
+        }}
+      />
+      <div className="p-2">
+        <h2>🏚️ Cômodo: {room.roomType}</h2>
+        <h2>🎨 Estilo: {room.designType}</h2>
+      </div>
+
+      <AiOutputDialog
+        isOpen={openOutputDialog}
+        setIsOpen={() => setOpenOutputDialog(false)}
+        orgImageUrl={room.orgImage}
+        aiImageUrl={room.aiImage}
+      />
+    </div>
+  );
+};
